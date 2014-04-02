48db73b92b438f23d53e38e766262df06804eb6d
Rekha <rajrekha85@gmail.com>
2014-04-02 11:34:26 -0500

    Made Changes

--- Read_Windx_netcdf.cpp
+++ Read_Windx_netcdf.cpp
@@ -27,13 +27,10 @@ int main()
 	int nele = 0;
 	int node = 0;
 
+	/*string xs, ys;*/
+
 	vector<string> attnames;
-	attnames.push_back("windx");	
-
-	vector<boost::shared_array<float>> depth;
-
-	for (int i = 0; i < attnames.size(); ++i)
-		depth.push_back(boost::shared_array<float>(new float[node]));	
+	attnames.push_back("windx");	 
 
 	if ((retval = nc_open(FILE_NAME, NC_NOWRITE, &ncid)))
 		return false;
@@ -53,6 +50,17 @@ int main()
 			node = lenp;		
 	}
 
+
+	/*boost::shared_array<int> ele;
+	boost::shared_array<double> x = boost::shared_array<double>(new double[node]);
+	boost::shared_array<double> y = boost::shared_array<double>(new double[node]);*/
+	vector<boost::shared_array<float>> depth;
+
+	for (int i = 0; i < attnames.size(); ++i)
+		depth.push_back(boost::shared_array<float>(new float[node]));	
+
+	/*ele = boost::shared_array<int>(new int[nele*3]);*/
+
 	for (int i = 0; i < nvars; ++i)
 	{
 		using namespace std;
@@ -67,17 +75,18 @@ int main()
 				{
 					if (attnames[j] == "windx")
 					{	
-						size_t end[] = {1,node};
-						size_t start[] = {timestep,0};
+						size_t end[] = {95,1};
+						size_t start[] = {1,0};
 						nc_get_vara_float (ncid, i, start, end, depth[j].get());
-
-						float *bptr=depth[j].get();
-
-						for ( int i = 0; i < 5; i++ )
+						
+						//boost::shared_array<float> wind=depth[0].get();
+						boost::shared_array<float> sa_wind= depth[0];
+						for ( int z = 0; z < 10; z++ )
 						{
 							std::cout<<setprecision(3);
-							cout << "*(bptr + " << i << ") : ";
-							cout << *(bptr + i) << endl;
+							cout<<"\n";
+							//cout << "*(bptr + " << z << ") : ";
+							cout << sa_wind[z];
 						}
 
 						found = true;
@@ -85,10 +94,63 @@ int main()
 
 				}
 			}
+			/*if (!found)
+			{
+				if (finder.find(xs) != -1 && finder.size() == xs.size())
+				{
+					size_t end = node;
+					size_t start = 0;
+					nc_get_vara_double (ncid, i, &start, &end, x.get());
+				}
+				else if (finder.find(ys) != -1 && finder.size() == xs.size())
+				{
+					size_t end = node;
+					size_t start = 0;
+					nc_get_vara_double (ncid, i, &start, &end, y.get());
+				}
+				else if (finder.find("element") != -1)
+				{
+					size_t end[] = {nele,3};
+					size_t start[] = {0,0};
+					nc_get_vara_int (ncid, i, start, end, ele.get());
+				}
+			}*/
 		}		 
 	}
 
-	boost::shared_array<int> ele = boost::shared_array<int>(new int[nele]);
+
+
+	//boost::shared_array<int> ele = boost::shared_array<int>(new int[nele]);
+
+	//for (int i = 1; i < nele/3; i++)
+	//{
+	//	int index = i*3;
+	//	int ind = ele.get()[index]-1;
+	//	double x1 = x.get()[ind];
+	//	double y1 = y.get()[ind];
+	//	double z1 = depth[0].get()[ind];
+	//	cout<<"z1  "<<z1;
+	//	index++;
+	//	ind = ele.get()[index]-1;
+	//	double x2 = x.get()[ind];
+	//	double y2 = y.get()[ind];
+	//	double z2 = depth[0].get()[ind];
+	//	cout<<"z2  "<<z2;
+	//	index++;
+	//	ind = ele.get()[index]-1;
+	//	double x3 = x.get()[ind];
+	//	double y3 = y.get()[ind];
+	//	double z3 = depth[0].get()[ind];
+	//	cout<<"z3   "<<z3;
+	//	/*if (z1 > -999 && z2 > -999 && z3 > -999)
+	//	{
+	//	fout << "v " << x1 << " " << z1 << " " << y1 << endl;
+	//	fout << "v " << x2 << " " << z2 << " " << y2 << endl;
+	//	fout << "v " << x3 << " " << z3 << " " << y3 << endl;
+	//	fout << "f " << count << " " << count + 1 << " " << count + 2 << endl;
+	//	count += 3;
+	//	}*/
+	//}
 
 
 
